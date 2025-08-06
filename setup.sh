7517a8995bcbc94bfa3a98cc15be7aa7f8925860
janselE <jansel.herrera@gmail.com>
2025-08-05 19:39:17 -0500

    adding a setup file to set the local aliases

---
+++ setup.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# Build the docker image
+docker build -t dev:1.0.0 .
+
+#!/bin/sh
+
+BASHRC="$HOME/.bashrc"
+BLOCK_START="# >>> docker dev-env aliases >>>"
+BLOCK_END="# <<< docker dev-env aliases <<<"
+
+# adds this section only if it has not been added yet
+if ! grep -q "$BLOCK_START" "$BASHRC"; then
+  cat << EOF >> "$BASHRC"
+$BLOCK_START
+alias dstart='
+  # Remove existing container if it exists (ensures no outdated container remains)
+  if docker ps -a --format "{{.Names}}" | grep -q "^dev-env$"; then
+    docker rm -f dev-env >/dev/null 2>&1
+  fi
+
+  # Start a new container
+  docker run -dit \
+    --name dev-env \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v \$HOME:\$HOME \
+    -v /mnt:/mnt \
+    -w "\$(pwd)" \
+    dev:1.0.0 bash
+
+  # Attach to the container
+  # docker exec -it dev-env bash
+'
+
+vim() {
+    docker exec -it -w "\$(pwd)" dev-env nvim "\$@"
+}
+
+nvim() {
+    docker exec -it -w "\$(pwd)" dev-env nvim "\$@"
+}
+
+tmux() {
+    docker exec -it -w "\$(pwd)" dev-env tmux -2 "\$@"
+}
+$BLOCK_END
+EOF
+fi
+
+source ~/.bashrc
