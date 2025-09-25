bb669b208a7c8ade4641694c0a4c7736dd7b9a51
Jansel <jansel.herrera@gmail.com>
2025-09-25 10:48:55 -0500

    adding a new pluging to move around 'flash' and removing 'hop'

---
+++ nvim/lua/user/plugins/flash.lua
@@ -0,0 +1,33 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- optional: tweak the labels and search mode
+    labels = "asdfghjklqwertyuiopzxcvbnm", -- similar feel to Hop
+    search = {
+      multi_window = false,
+      forward = true,
+      wrap = true,
+      mode = "exact",
+    },
+    jump = {
+      nohlsearch = true,
+      autojump = true,
+    },
+  },
+  keys = {
+    -- behave like hop.nvim’s anywhere jump
+    { "<leader>jk", mode = { "n", "x", "o" },
+      function() require("flash").jump() end,
+      desc = "Flash Jump Anywhere"
+    },
+
+    -- optional extras if you want
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+}
+
--- nvim/lua/user/plugins/lsp/lspconfig.lua
+++ nvim/lua/user/plugins/lsp/lspconfig.lua
@@ -6,100 +6,52 @@ return {
     { "antosha417/nvim-lsp-file-operations", config = true },
   },
   config = function()
-    -- import lspconfig plugin
-    local lspconfig = require("lspconfig")
-
-    -- import cmp-nvim-lsp plugin
+    local keymap = vim.keymap
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-    local keymap = vim.keymap -- for conciseness
-
-    local opts = { noremap = true, silent = true }
-    local on_attach = function(client, bufnr)
-      opts.buffer = bufnr
-
-      -- set keybinds
-      opts.desc = "Show LSP references"
-      keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-
-      opts.desc = "Go to declaration"
-      keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-
-      opts.desc = "Show LSP definitions"
-      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-      opts.desc = "Show LSP implementations"
-      keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
-      opts.desc = "Show LSP type definitions"
-      keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-      opts.desc = "See available code actions"
-      keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-      opts.desc = "Smart rename"
-      keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
-      opts.desc = "Show buffer diagnostics"
-      keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-      opts.desc = "Show line diagnostics"
-      keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-      opts.desc = "Go to previous diagnostic"
-      keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-      opts.desc = "Go to next diagnostic"
-      keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-      opts.desc = "Show documentation for what is under cursor"
-      keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-      opts.desc = "Restart LSP"
-      keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
-    end
-
-    -- used to enable autocompletion (assign to every lsp server config)
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    -- Change the Diagnostic symbols in the sign column (gutter)
-    -- (not in youtube nvim video)
+    local on_attach = function(client, bufnr)
+      local opts = { noremap = true, silent = true, buffer = bufnr }
+
+      -- LSP navigation and actions
+      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+      keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+      keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+      keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+      keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+      keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+      keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+      keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+      keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+      keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+      keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+      keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+    end
+
+    -- Customize diagnostic symbols
     local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
     for type, icon in pairs(signs) do
       local hl = "DiagnosticSign" .. type
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
 
-    -- configure html server
-    lspconfig["html"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
+    -- LSP server setups
+    local lspconfig = require("lspconfig")
 
-    -- configure typescript server with plugin
-    lspconfig["ts_ls"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
-
-    -- configure css server
-    lspconfig["cssls"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
-
-    -- configure tailwindcss server
-    lspconfig["tailwindcss"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
-
-    -- configure svelte server
-    lspconfig["svelte"].setup({
+    -- HTML
+    lspconfig.html.setup({ capabilities = capabilities, on_attach = on_attach })
+    -- TypeScript
+    lspconfig.ts_ls.setup({ capabilities = capabilities, on_attach = on_attach })
+    -- CSS
+    lspconfig.cssls.setup({ capabilities = capabilities, on_attach = on_attach })
+    -- TailwindCSS
+    lspconfig.tailwindcss.setup({ capabilities = capabilities, on_attach = on_attach })
+    -- Svelte
+    lspconfig.svelte.setup({
       capabilities = capabilities,
       on_attach = function(client, bufnr)
         on_attach(client, bufnr)
-
         vim.api.nvim_create_autocmd("BufWritePost", {
           pattern = { "*.js", "*.ts" },
           callback = function(ctx)
@@ -110,45 +62,30 @@ return {
         })
       end,
     })
-
-    -- configure prisma orm server
-    lspconfig["prismals"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
-
-    -- configure graphql language server
-    lspconfig["graphql"].setup({
+    -- Prisma
+    lspconfig.prismals.setup({ capabilities = capabilities, on_attach = on_attach })
+    -- GraphQL
+    lspconfig.graphql.setup({
       capabilities = capabilities,
       on_attach = on_attach,
       filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
     })
-
-    -- configure emmet language server
-    lspconfig["emmet_ls"].setup({
+    -- Emmet
+    lspconfig.emmet_ls.setup({
       capabilities = capabilities,
       on_attach = on_attach,
       filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
     })
-
-    -- configure python server
-    lspconfig["pyright"].setup({
+    -- Python
+    lspconfig.pyright.setup({ capabilities = capabilities, on_attach = on_attach })
+    -- Lua
+    lspconfig.lua_ls.setup({
       capabilities = capabilities,
       on_attach = on_attach,
-    })
-
-    -- configure lua server (with special settings)
-    lspconfig["lua_ls"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = { -- custom settings for lua
+      settings = {
         Lua = {
-          -- make the language server recognize "vim" global
-          diagnostics = {
-            globals = { "vim" },
-          },
+          diagnostics = { globals = { "vim" } },
           workspace = {
-            -- make language server aware of runtime files
             library = {
               [vim.fn.expand("$VIMRUNTIME/lua")] = true,
               [vim.fn.stdpath("config") .. "/lua"] = true,
@@ -159,3 +96,4 @@ return {
     })
   end,
 }
+
